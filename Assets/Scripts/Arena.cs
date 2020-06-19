6c21b06196dc4b137c1f3e82c738ff4aa131a596
Reinaldo Ramos <masterrey@gmail.com>
2020-06-18 21:22:38 -0300

    tanques

---
+++ Assets/Scripts/Arena.cs
@@ -0,0 +1,20 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arena : MonoBehaviour
+{
+    public GameObject respawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.Instantiate("TankFree_Blue", respawn.transform.position,respawn.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,32 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public Rigidbody rdb;
+    public PhotonView pview;
+    Vector3 playercommand,playertorque;
+    public float motorpower=100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!pview.IsMine)
+        {
+            enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playercommand=new Vector3(0, 0, Input.GetAxis("Vertical"));
+        playertorque = new Vector3(0, Input.GetAxis("Horizontal"), 0);
+    }
+    private void FixedUpdate()
+    {
+        rdb.AddRelativeForce(playercommand * motorpower);
+        rdb.AddTorque(playertorque * motorpower);
+    }
+}
