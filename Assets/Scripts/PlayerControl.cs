320141a2fb864ddc583b230beeffc6b07dbcde2f
Reinaldo Ramos <masterrey@gmail.com>
2020-06-25 19:50:40 -0300

    cores

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -9,9 +9,11 @@ public class PlayerControl : MonoBehaviour
     public PhotonView pview;
     Vector3 playercommand,playertorque;
     public float motorpower=100;
+    public MeshRenderer rend;
     // Start is called before the first frame update
     void Start()
     {
+        rend.material.color = GetColor(pview.Owner.ActorNumber);
         if (!pview.IsMine)
         {
             enabled = false;
@@ -32,5 +34,21 @@ public class PlayerControl : MonoBehaviour
     {
         rdb.AddRelativeForce(playercommand * motorpower);
         rdb.AddTorque(playertorque * motorpower);
+    }
+    Color GetColor(int colorChoice)
+    {
+        switch (colorChoice)
+        {
+            case 0: return Color.red;
+            case 1: return Color.green;
+            case 2: return Color.blue;
+            case 3: return Color.yellow;
+            case 4: return Color.cyan;
+            case 5: return Color.grey;
+            case 6: return Color.magenta;
+            case 7: return Color.white;
+        }
+
+        return new Color(Random.Range(0,1), colorChoice * 0.1f, Random.Range(0, 1));
     }
 }
